2722e815d5075f609b8308991810ccad68b8956e
jdsolo <120941334+jdsolo@users.noreply.github.com>
2025-11-13 06:47:48 +0000

    Deploying to gh-pages from @ jdsolo/jdsolo.github.io@c56e2c2d193424eebf427b61fb0001512b88c74f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -460,13 +460,6 @@ ninja.data = [{
           window.open("https://www.researchgate.net/profile/Jonathon_Tanks/", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
