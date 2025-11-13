8cce8b1bf22492dc8c42d449be2e2f10f8128f90
jdsolo <120941334+jdsolo@users.noreply.github.com>
2025-11-13 05:31:59 +0000

    Deploying to gh-pages from @ jdsolo/jdsolo.github.io@8fef86ff8b3b9b787eecc469a3ba15a295a12550 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,14 +443,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%74%61%6E%6B%73[.]%6A%6F%6E%61%74%68%6F%6E[@]%6E%69%6D%73[.]%67%6F[.]%6A%70", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://orcid.org/0000-0002-0232-8240", "_blank");
+        },
+      },{
+        id: 'social-researchgate',
+        title: 'ResearchGate',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.researchgate.net/profile/Jonathon_Tanks/", "_blank");
         },
       },{
         id: 'social-rss',
@@ -464,14 +471,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=m8xHhkAAAAJ&hl", "_blank");
         },
       },{
       id: 'light-theme',
