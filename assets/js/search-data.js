75fda28e8b502a305b42f72a1c74a6da6bc96f04
jdsolo <120941334+jdsolo@users.noreply.github.com>
2025-11-14 05:43:25 +0000

    Deploying to gh-pages from @ jdsolo/jdsolo.github.io@368b12e3433fb6cecefb670f86f7e3595e452c9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "My full CV is available on this webpage, or you can download the PDF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
