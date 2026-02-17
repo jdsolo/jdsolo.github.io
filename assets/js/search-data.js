451dd8bcfd7bfd71d686b89d25cd579b48942d50
jdsolo <120941334+jdsolo@users.noreply.github.com>
2026-02-17 05:54:38 +0000

    Deploying to gh-pages from @ jdsolo/jdsolo.github.io@cd1c7cf326c973452f2f248584e760333ccbc331 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -433,7 +433,7 @@ ninja.data = [{
               window.location.href = "/projects/4_project/";
             },},{id: "projects-nims-denka-center-of-excellence-for-next-gen-materials-collaboration",
           title: 'NIMS-DENKA Center of Excellence for Next-Gen Materials - Collaboration',
-          description: "Creation of High-Performance Functional Hybrid Fibers",
+          description: "Functional Materials",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
